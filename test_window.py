fa7c4439d5502f94c071240c735fe20653047115
Troy Martin <beef.erikson.studios@gmail.com>
2023-10-31 01:51:24 -0400

    Added return statements to test functions.

--- test_window.py
+++ test_window.py
@@ -1,12 +1,12 @@
 import window
 
 
-def test_create_console():
+def test_create_console() -> None:
     assert str(type(
         window.create_console(500, 400))) == "<class 'tcod.console.Console'>"
 
 
-def test_create_tileset():
+def test_create_tileset() -> None:
     assert (str(type(window.create_tileset('assets/Cheepicus_14x14.png', 16, 16)))
             == "<class 'tcod.tileset.Tileset'>")
 
