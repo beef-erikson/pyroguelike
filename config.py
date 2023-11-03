23dfa3b0c9f3b55360c6de5d8e23e50690066b83
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-03 12:32:08 -0400

    Linting fix.

--- config.py
+++ config.py
@@ -5,4 +5,4 @@
 DEBUG = True  # Debug flag to display debug messages.
 WIDTH = 80  # Number of tiles wide.
 HEIGHT = 50  # Number of tiles high.
-EIGHT_WAY_CONTROL = False  # Set to True for diagonal control.
+EIGHT_WAY_CONTROL = True  # Set to True for diagonal control.
--- event_actions.py
+++ event_actions.py
@@ -1,18 +1,21 @@
-from attrs import define
 """
     Used in conjunction with the event handler to define action classes we can use elsewhere.
 """
+from attrs import define
 
 
 class Action:
+    """ Used for all handling. """
     pass
 
 
 class EscapeAction(Action):
+    """ Used when a 'quit' key is pressed. """
     pass
 
 
 @define
 class MovementAction(Action):
+    """ Used when a movement key is pressed. """
     x: int
     y: int
