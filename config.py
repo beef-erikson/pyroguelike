afaec63b0c694ba2aa7b69d5aaea7e613740c54f
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-03 02:02:46 -0400

    8-Way controls added. Flag in config.py can toggle between 4-way and 8-way.

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
     CONFIGURATIONS - Change at will
 """
 
-debug = True  # Debug flag to enable additional messing.
+debug = True  # Debug flag to display debug messages.
 width = 80  # Number of tiles wide.
 height = 50  # Number of tiles high.
+eight_way_control = True  # Set to True for diagonal control.
--- event_handler.py
+++ event_handler.py
@@ -1,3 +1,4 @@
+import config
 from event_actions import Action, EscapeAction, MovementAction
 import tcod.event
 
@@ -16,18 +17,38 @@ class EventHandler(tcod.event.EventDispatch[Action]):
         action = None
         key = event.sym
 
-        # Player movement
-        if key == key.LEFT or key == key.a or key == key.KP_4:
-            action = MovementAction(x=-1, y=0)
-        elif key == key.RIGHT or key == key.d or key == key.KP_6:
-            action = MovementAction(x=1, y=0)
-        elif key == key.UP or key == key.w or key == key.KP_8:
-            action = MovementAction(x=0, y=-1)
-        elif key == key.DOWN or key == key.s or key == key.KP_2:
-            action = MovementAction(x=0, y=1)
+        # Player movement: 4-Way controls
+        if not config.eight_way_control:
+            if key == key.LEFT or key == key.a or key == key.KP_4:
+                action = MovementAction(x=-1, y=0)
+            elif key == key.RIGHT or key == key.d or key == key.KP_6:
+                action = MovementAction(x=1, y=0)
+            elif key == key.UP or key == key.w or key == key.KP_8:
+                action = MovementAction(x=0, y=-1)
+            elif key == key.DOWN or key == key.s or key == key.KP_2:
+                action = MovementAction(x=0, y=1)
+        # Player movement: 8-Way controls
+        else:
+            match key:
+                case key.KP_1:
+                    action = MovementAction(x=-1, y=1)  # SW
+                case key.KP_2:
+                    action = MovementAction(x=0, y=1)  # S
+                case key.KP_3:
+                    action = MovementAction(x=1, y=1)  # SE
+                case key.KP_4:
+                    action = MovementAction(x=-1, y=0)  # W
+                case key.KP_6:
+                    action = MovementAction(x=1, y=0)  # E
+                case key.KP_7:
+                    action = MovementAction(x=-1, y=-1)  # NW
+                case key.KP_8:
+                    action = MovementAction(x=0, y=-1)  # N
+                case key.KP_9:
+                    action = MovementAction(x=1, y=-1)  # NE
 
         # Quit the game
-        elif key == key.ESCAPE or key == key.q:
+        if key == key.ESCAPE or key == key.q:
             action = EscapeAction()
 
         return action
