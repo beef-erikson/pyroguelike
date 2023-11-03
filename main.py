c8df614c3513ac10ea42b019fb275ffc392e2177
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-03 02:37:18 -0400

    # TODO added.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main() -> None:
     console = window.create_console()
     tile_set = window.create_tile_set('assets/Cheepicus_14x14.png', 16, 16)
 
+    # TODO - Implement a MobFactory or something for spawning these in a 'better' way.
     # Characters
     player = Player(x=console.width // 2, y=console.height // 2, icon='@', movement_speed=1, color=(25, 255, 255))
     npc = Entity(x=15, y=20, icon='&', movement_speed=1, color=(44, 255, 33))
