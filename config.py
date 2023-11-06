c0510b5f1ada4f5bde8b1a03bba44e200f757773
Troy Martin <beef.erikson.studios@gmail.com>
2023-11-06 00:15:59 -0500

    "Map" creation partially working. Need to get walls working now. MAP_BUFFER_X and MAP_BUFFER_Y properties added to config.py.

--- config.py
+++ config.py
@@ -2,12 +2,19 @@
     CONFIGURATIONS - Change at will
 """
 
+""" MISC DISPLAY AND DEBUG """
 DEBUG = True  # Debug flag to display debug messages.
 SHOW_TIME = True  # Set to False to disable time display.
 
+""" WINDOW PROPERTIES """
 WIDTH = 80  # Number of tiles wide.
 HEIGHT = 50  # Number of tiles high.
-MAP_WIDTH = 45  # Number of map tiles wide.
-MAP_HEIGHT = 45  # Number of map tiles high.
 
+""" MAP PROPERTIES """
+MAP_BUFFER_X = 0
+MAP_BUFFER_Y = 3
+MAP_WIDTH = 80  # Number of map tiles wide.
+MAP_HEIGHT = 44  # Number of map tiles high.
+
+""" CONTROL """
 EIGHT_WAY_CONTROL = True  # Set to True for diagonal control.
--- map/main_map.py
+++ map/main_map.py
@@ -1,10 +1,15 @@
 """ Code to handle map bounds and rendering. """
 from numpy import full
 from tcod.console import Console
-
 import config
 from map import tiles
-from process import color
+
+
+def render_map(console: Console) -> None:
+    """ Renders the map to fill with tiles """
+    console.rgba[config.MAP_BUFFER_Y:config.MAP_HEIGHT, config.MAP_BUFFER_X:config.MAP_WIDTH]\
+        = tiles.standard_floor['dark']
+
 
 class MainMap:
     """ MainMap class used for making the dungeon maps. """
@@ -18,11 +23,3 @@ class MainMap:
     def is_in_bounds(self, x: int, y: int):
         """ Checks to see if entity is in bounds of the map. """
         return 0 <= x < self.width and 0 <= y < self.height
-
-    # TODO - Left off here.
-    def render_map(self, console: Console) -> None:
-        """ Renders the map to fill with tiles """
-        console.rgba[0:config.MAP_WIDTH, 0:config.MAP_HEIGHT] = tiles.standard_floor['dark']
-            # (ord(' '), color.get_rgba(color.BLACK, 255), color.get_rgba(color.DARK_BLUE, 255))
-
-# tiles.standard_floor['dark']
